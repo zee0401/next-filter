56dd6bf22edbac6830c7ddab72c26fd3546d9e11
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-09-09 12:28:59 +0530

    Products components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,6 +88,9 @@ export default function Home() {
           </button>
         </div>
       </div>
+      <section className="pb-24 pt-6">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4"></div>
+      </section>
     </main>
   );
 }
